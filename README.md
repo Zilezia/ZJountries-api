da40d5e76b07e51ea12cfb7c8cedf9cb06c9a247
Zilezia <czeriwanczak@gmail.com>
2024-07-06 03:59:05 +0100

    full name calling

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ZJountries-api
 My own rest api for countries, will be used in [this](https://github.com/Zilezia/ZJountries/)
 
+<!-- gotta somehow fix languages being scraped -->
+<!-- scrape from wpedia instead wdata -->
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 from flask_restful import Resource, Api
 import pandas as pd
 
@@ -21,51 +21,57 @@ class CountryName(Resource):
     def get(self, names):
         names_list = names.split(',')
         matching_countries = []
+        
+        full_text_call = request.args.get('fulltext', '').lower() == 'true'
+        
         for name in names_list:
             name_lower = name.lower()
-            matching_countries.extend(
-                [country for country in data if name_lower in country['name']['common'].lower()]
-            )
+            if full_text_call:
+                matches = [country for country in data if name_lower == country['name']['common'].lower()]
+            else:
+                matches = [country for country in data if name_lower in country['name']['common'].lower()]
+            matching_countries.extend(matches)
+            
         if matching_countries:
             return jsonify(matching_countries)
         else:
             return jsonify({"error": "Country not found."}), 404
 
-class AlphaTwoCode(Resource):
+class ISO2Code(Resource):
     def get(self, alpha2s):
         alpha2_list = alpha2s.split(',')
         matching_countries = []
         for alpha2 in alpha2_list:
             alpha2_lower = alpha2.lower()
             matching_countries.extend(
-                [country for country in data if alpha2_lower in country['code']['alpha-2'].lower()]
+                [country for country in data if alpha2_lower == country['code']['alpha-2'].lower()]
             )
         if matching_countries:
             return jsonify(matching_countries)
         else:
             return jsonify({"error": "Country not found."}), 404
         
-class AlphaThreeCode(Resource):
+class ISO3Code(Resource):
     def get(self, alpha3s):
         alpha3_list = alpha3s.split(',')
         matching_countries = []
         for alpha3 in alpha3_list:
             alpha3_lower = alpha3.lower()
             matching_countries.extend(
-                [country for country in data if alpha3_lower in country['code']['alpha-3'].lower()]
+                [country for country in data if alpha3_lower == country['code']['alpha-3'].lower()]
             )
         if matching_countries:
             return jsonify(matching_countries)
         else:
             return jsonify({"error": "Country not found."}), 404
         
-class NumericCode(Resource):
+class ISONumCode(Resource):
     def get(self, numerics):
         numeric_list = numerics.split(',')
         matching_countries = []
         for numeric in numeric_list:
             matching_countries.extend(
-                [country for country in data if numeric in country['code']['numeric']]
+                [country for country in data if numeric == country['code']['numeric']]
             )
         if matching_countries:
             return jsonify(matching_countries)
@@ -80,11 +86,11 @@ def inject_version():
 def home():
     return render_template("index.html")
 
-api.add_resource(Countries,     f"/{api_ver}/all")
-api.add_resource(CountryName,   f"/{api_ver}/name=<string:names>")
-api.add_resource(AlphaTwoCode,  f"/{api_ver}/iso2=<string:alpha2s>")
-api.add_resource(AlphaThreeCode,f"/{api_ver}/iso3=<string:alpha3s>")
-api.add_resource(NumericCode,   f"/{api_ver}/isoN=<string:numerics>")
+api.add_resource(Countries, f"/{api_ver}/all")
+api.add_resource(CountryName, f"/{api_ver}/name=<string:names>")
+api.add_resource(ISO2Code, f"/{api_ver}/iso2=<string:alpha2s>")
+api.add_resource(ISO3Code, f"/{api_ver}/iso3=<string:alpha3s>")
+api.add_resource(ISONumCode, f"/{api_ver}/isoN=<string:numerics>")
 
 
 if __name__ == '__main__':
